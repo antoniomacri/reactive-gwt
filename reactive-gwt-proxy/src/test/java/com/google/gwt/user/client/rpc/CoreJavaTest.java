36f5eae4fb4682af0c225924a0b1b1639d08fb4e
Antonio Macrì <ing.antonio.macri@gmail.com>
2023-09-26 12:21:05 +0200

    Fix CoreJavaTest

--- reactive-gwt-proxy/src/test/java/com/google/gwt/user/client/rpc/CoreJavaTest.java
+++ reactive-gwt-proxy/src/test/java/com/google/gwt/user/client/rpc/CoreJavaTest.java
@@ -1,73 +1,75 @@
-///*
-// * Copyright 2011 Google Inc.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// * use this file except in compliance with the License. You may obtain a copy of
-// * the License at
-// *
-// * http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// * License for the specific language governing permissions and limitations under
-// * the License.
-// */
-//
-//package com.google.gwt.user.client.rpc;
-//
-//import java.math.MathContext;
-//import java.math.RoundingMode;
-//
-//import com.github.antoniomacri.reactivegwt.proxy.SyncProxy;
-//
-///**
-// * Test cases for Java core emulation classes in GWT RPC.
-// *
-// * Logging is tested in a separate suite because it requires logging enabled in
-// * gwt.xml. Otherwise, only MathContext has non-trivial content for RPC.
-// *
-// * Modified by P.Prith in 0.5 to utilize Local App Engine server for service
-// * through SyncProxy against Test in GWT 2.7.0
-// *
-// */
-//public class CoreJavaTest extends RpcTestBase {
-//
-//	private static MathContext createMathContext() {
-//		return new MathContext(5, RoundingMode.CEILING);
-//	}
-//
-//	public static boolean isValid(MathContext value) {
-//		return createMathContext().equals(value);
-//	}
-//
-//	private CoreJavaTestServiceAsync coreJavaTestService;
-//
-//	private CoreJavaTestServiceAsync getServiceAsync() {
-//		if (this.coreJavaTestService == null) {
-//			this.coreJavaTestService = SyncProxy
-//					.create(CoreJavaTestService.class);
-//		}
-//		return this.coreJavaTestService;
-//	}
-//
-//	public void testMathContext() {
-//		CoreJavaTestServiceAsync service = getServiceAsync();
-//		final MathContext expected = createMathContext();
-//
-//		delayTestFinishForRpc();
-//		service.echoMathContext(expected, new AsyncCallback<MathContext>() {
-//			@Override
-//			public void onFailure(Throwable caught) {
-//				TestSetValidator.rethrowException(caught);
-//			}
-//
-//			@Override
-//			public void onSuccess(MathContext result) {
-//				assertNotNull(result);
-//				assertTrue(isValid(result));
-//				finishTest();
-//			}
-//		});
-//	}
-//}
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gwt.user.client.rpc;
+
+import com.google.gwt.user.server.rpc.CollectionsTestServiceImpl;
+import com.google.gwt.user.server.rpc.CoreJavaTestServiceImpl;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Test;
+
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Test cases for Java core emulation classes in GWT RPC.
+ * <p>
+ * Logging is tested in a separate suite because it requires logging enabled in
+ * gwt.xml. Otherwise, only MathContext has non-trivial content for RPC.
+ * <p>
+ * Modified by Antonio Macrì to perform tests against an embedded Jetty with the GWT servlet.
+ */
+public class CoreJavaTest extends RpcTestBase<CoreJavaTestService, CoreJavaTestServiceAsync> {
+
+    @Deployment(testable = false)
+    @SuppressWarnings("unused")
+    public static WebArchive getTestArchive() {
+        return buildTestArchive(CoreJavaTestServiceImpl.class, "corejava",
+                "AppModule.nocache.js",
+                "DD41F2E9EE38B6CC75CBEB6AD11D073C.cache.js",
+                "compilation-mappings.txt",
+                "E5B982CE4D10194FA6077F74B63B92E0.gwt.rpc"
+        );
+    }
+
+    private static MathContext createMathContext() {
+        return new MathContext(5, RoundingMode.CEILING);
+    }
+
+    public static boolean isValid(MathContext value) {
+        return createMathContext().equals(value);
+    }
+
+
+    public CoreJavaTest() {
+        super(CoreJavaTestService.class);
+    }
+
+
+    @Test
+    public void testMathContext() {
+        final MathContext expected = createMathContext();
+        service.echoMathContext(expected, createCallback(result -> {
+            assertNotNull(result);
+            assertTrue(isValid(result));
+        }));
+    }
+}
